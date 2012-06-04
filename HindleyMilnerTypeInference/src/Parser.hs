39adfc99698d6ca7c0886af3ddb9eff16554ea66
Edmondo <edmondo@gmail.com>
2012-06-04 23:39:25 +0100

    Added more combinators from Monadic Parsing in Haskell paper

--- HindleyMilnerTypeInference/src/Parser.hs
+++ HindleyMilnerTypeInference/src/Parser.hs
@@ -1,7 +1,12 @@
 module Parser where
 
-import Prelude (Monad, String, concat, return, (>>=), Char, Bool, (==))
+import Prelude (Monad, String, concat, return, (>>=), Char, Bool, Int, (==), (-), (+), (*), div)
+import Data.Char (isSpace, isDigit, ord)
   
+-- Monadic Parsing in Haskell
+-- Graham Hutton - University of Nottingham
+-- Erik Meijer - University of Utrecht
+
 data Parser a = Parser (String -> [(a, String)])
 
 unit :: a -> Parser a
@@ -37,7 +42,9 @@ sat :: (Char -> Bool) -> Parser Char
 sat p = do { c <- item; if p c then return c else zero }    
 
 char :: Char -> Parser Char
-char c = sat (c ==)                                                                   
+char c = sat (c ==)      
+
+-- Recursion combinators                                                             
               
 string :: String -> Parser String
 string "" = return ""
@@ -54,5 +61,51 @@ p `sepBy` sep = p `sepBy1` sep +++ return []
 
 sepBy1 :: Parser a -> Parser a -> Parser [a]
 p `sepBy1` sep = do x <- p
-                    xs <- many (do { _ <- sep; p });
-                    return (x:xs)                                  
+                    xs <- many (do { _ <- sep; p })
+                    return (x:xs)                  
+                    
+chainl :: Parser a -> Parser (a -> a -> a) -> a -> Parser a
+chainl p op a = (p `chainl1` op) +++ return a
+
+chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
+p `chainl1` op = do { a <- p; rest a }
+                 where
+                    rest a = (do f <- op
+                                 b <- p
+                                 rest (f a b))
+                             +++ return a             
+                             
+-- Lexical combinators                       
+
+space :: Parser String
+space = many (sat isSpace)    
+
+token :: Parser a -> Parser a
+token p = do { a <- p; _ <- space; return a }    
+
+symb :: String -> Parser String
+symb cs = token (string cs)             
+
+apply :: Parser a -> String -> [(a, String)]
+apply p = parse (do {_ <- space; p})        
+
+-- Example
+
+expr :: Parser Int
+expr = term `chainl1` addop
+
+term :: Parser Int
+term = factor `chainl1` mulop
+
+factor :: Parser Int
+factor = digit +++ do { _ <- symb "("; n <- expr; _ <- symb ")"; return n }
+
+digit :: Parser Int
+digit = do { x <- token (sat isDigit); return (ord x - ord '0')}
+
+addop :: Parser (Int -> Int -> Int)
+addop = do { _ <- symb "+" ; return (+)} +++ do { _ <- symb "-"; return (-)}
+
+mulop :: Parser (Int -> Int -> Int)
+mulop = do { _ <- symb "*" ; return (*)} +++ do { _ <- symb "/"; return (div)}
+
